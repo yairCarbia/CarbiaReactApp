78fec8ad1a588173fdf285889ac1cb58f3716f4b
zarpadoen <yaircarbia420@gmail.com>
2022-04-25 21:35:40 -0300

    Desafio CLI

---
+++ carbia-app/src/index.js
@@ -0,0 +1,9 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+const saludar = <h1>Hola Mundo</h1>;
+
+
+const div = document.querySelector("#root");
+
+ReactDOM.render(saludar, div);
