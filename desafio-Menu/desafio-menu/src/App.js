3ea23cef0ef47a926223efb31cea8e4757f54a98
zarpadoen <yaircarbia420@gmail.com>
2022-05-01 10:10:36 -0300

    desafio/Menu

---
+++ desafio-Menu/desafio-menu/src/App.js
@@ -0,0 +1,14 @@
+import logo from './logo.svg';
+
+import Boostrap from './components/Boostrap';
+function App() {
+  return (
+    <div>
+      <Boostrap />
+      {/* <h2> MENU</h2> */}
+
+    </div>
+  );
+}
+
+export default App;
---
+++ desafio-Menu/desafio-menu/src/components/Boostrap.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import NavBar from './NavBar';
+function Boostrap() {
+    return (
+        <>
+            <NavBar />
+            <h3>Boostrap</h3>
+
+        </>
+
+    )
+}
+
+export default Boostrap;
